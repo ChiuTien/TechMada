a9b9f2a099c9df1e3f986b024e6d6b5908c71f4f
VlexandreMG <dodolashmou@gmail.com>
2026-05-21 15:49:25 +0300

    Reglage sidebar nav

--- app/Views/employe/dashboard.php
+++ app/Views/employe/dashboard.php
@@ -13,10 +13,10 @@
     </div>
     <div class="sidebar-section">Menu</div>
     <ul class="sidebar-nav">
-      <li><a href="#page-dashboard-employe" class="active"><i class="bi bi-grid-1x2"></i> Tableau de bord</a></li>
-      <li><a href="#page-form-conge"><i class="bi bi-plus-circle"></i> Nouvelle demande</a></li>
+      <li><a href="<?= base_url('employe/dashboard') ?>" class="active"><i class="bi bi-grid-1x2"></i> Tableau de bord</a></li>
+      <li><a href="<?= base_url('employe/create') ?>"><i class="bi bi-plus-circle"></i> Nouvelle demande</a></li>
       <li>
-        <a href="#page-mes-conges">
+        <a href="<?= base_url('employe/index') ?>">
           <i class="bi bi-calendar3"></i> Mes demandes
           <span class="nav-badge alert">2</span>
         </a>
